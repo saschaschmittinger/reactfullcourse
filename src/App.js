971a8108b214b4f7d20a2ea3c702dd70d6e7963f
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-10-23 20:44:24 +0200

    23_10_24

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hallo Sascha! du musst dir den Scheiß reinziehen wenn du gleile Apps
+          machen willst
         </a>
       </header>
     </div>
